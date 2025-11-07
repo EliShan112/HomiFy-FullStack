b00df4b11fb9e838bb8227f390826c2bd2e28571
EliShan112 <asifkausar@email.com>
2025-11-07 23:46:34 +0530

    deploy fix 6

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -31,7 +32,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AuthProvider>
-          <Navbar/>
+          <Suspense fallback={<div>Loading navigation...</div>}>
+          <Navbar />
+        </Suspense>
           {children}
         </AuthProvider>
         <Footer/>
